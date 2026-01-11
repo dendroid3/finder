33190e2b58fb958a9c40fd22fb0d4bfc3c497e85
Wanjohi <kazibin66@gmail.com>
2026-01-11 10:11:31 +0300

    feat: Enable logout route and add styling to template 3

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ $app->getRouter()->get('/home', 'HomeController@index');
 
 $app->getRouter()->get('/register', 'RegistrationController@create');
 $app->getRouter()->post('/register', 'RegistrationController@store');
-//$app->getRouter()->get('/api/login', 'LoginController@create');
+$app->getRouter()->get('/login', 'LoginController@create');
 $app->getRouter()->post('/login', 'LoginController@store');
-
-// require_once __DIR__ . 'api.php';
+$app->getRouter()->post('/logout', 'LoginController@logout');
 
 $app->getRouter()->fallback('MainController@index');
 
